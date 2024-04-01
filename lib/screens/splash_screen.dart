739368fcdf1bfc558f0114a3eabf05d32fc08db7
TeeWrath <subroto.2003@gmail.com>
2024-04-01 11:17:37 +0530

    feat: Splash Screen Created

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:ayojaka/main.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(milliseconds: 4000), () {});
+    Navigator.pushReplacement(context,
+        MaterialPageRoute(builder: (BuildContext context) => const Scaffold(
+          body: Center(child: Text('Splash Screen Created', style: TextStyle(color: Colors.red),)),
+        )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 22, 22, 22),
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png',
+          width: 200,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
